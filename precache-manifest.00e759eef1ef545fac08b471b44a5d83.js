e47b8db69f1820346e5126975167d54cac7ce711
Pangilinank <40500163+Pangilinank@users.noreply.github.com>
2020-09-18 16:40:05 -0400

    Add files via upload

---
+++ precache-manifest.00e759eef1ef545fac08b471b44a5d83.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac04b8f077636389bd99c339307d22e",
+    "url": "/restaurant-project-app/index.html"
+  },
+  {
+    "revision": "6ac094c6d2207125bf63",
+    "url": "/restaurant-project-app/static/css/2.687a66c2.chunk.css"
+  },
+  {
+    "revision": "8268e9b5d9e312c617fb",
+    "url": "/restaurant-project-app/static/css/main.bf109335.chunk.css"
+  },
+  {
+    "revision": "6ac094c6d2207125bf63",
+    "url": "/restaurant-project-app/static/js/2.e7329f52.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/restaurant-project-app/static/js/2.e7329f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8268e9b5d9e312c617fb",
+    "url": "/restaurant-project-app/static/js/main.bb01087e.chunk.js"
+  },
+  {
+    "revision": "25495330fb96265e5166",
+    "url": "/restaurant-project-app/static/js/runtime-main.6179dcba.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/restaurant-project-app/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/restaurant-project-app/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "4939e3c8b6d0272ad6a5fcc22f17b60c",
+    "url": "/restaurant-project-app/static/media/bg1.4939e3c8.jpg"
+  },
+  {
+    "revision": "45fac22234b8613558976ac148ae7503",
+    "url": "/restaurant-project-app/static/media/bg2.45fac222.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/restaurant-project-app/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/restaurant-project-app/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/restaurant-project-app/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/restaurant-project-app/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/restaurant-project-app/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/restaurant-project-app/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/restaurant-project-app/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/restaurant-project-app/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/restaurant-project-app/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/restaurant-project-app/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/restaurant-project-app/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/restaurant-project-app/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/restaurant-project-app/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/restaurant-project-app/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/restaurant-project-app/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
